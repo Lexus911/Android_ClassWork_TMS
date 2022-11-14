8b831714ac350d47dc89aa717db487c51b125671
Alexey Matskevich <61157700+Lexus911@users.noreply.github.com>
2022-11-14 22:12:16 +0300

    Android lesson 2

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -2,17 +2,22 @@ package com.example.kotlinapp
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import com.example.kotlinapp.MainActivity2.Companion.startMainActivity2
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor(getString(R.string.blue))))
+        supportActionBar?.title = "Add contact"
 
         val btnGoToActivity2 = findViewById<Button>(R.id.btnGoToActivity2)
 
@@ -23,6 +28,16 @@ class MainActivity : AppCompatActivity() {
                 .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 //.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY) //MainActivity2 не будет добавлена в бэкстек
             )
+        }
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val btn = findViewById<Button>(R.id.btn)
+
+        btn.setOnClickListener{
+            textView.text = getString(R.string.Queen)
+
+            startMainActivity2(this, textView.text.toString() + getString(R.string.from_MainActivity))
+
         }
     }
 
--- app/src/main/java/com/example/kotlinapp/MainActivity3.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity3.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 
 class MainActivity3 : AppCompatActivity() {
@@ -11,10 +12,16 @@ class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
-
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         val btn = findViewById<Button>(R.id.btnGoToActivity1)
         btn.setOnClickListener{
             startActivity(Intent(this,MainActivity::class.java))
         }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == android.R.id.home){
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
